b94d3d5dad71a849f31bb2459c0137f7df2df0a4
Sushil Ghimire <sushil79g@gmail.com>
2019-11-24 17:11:12 +0545

    add new src folder to import files; learning modularity

---
+++ image_float/image_float/lib/src/app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: (){
+            print('Hi there!');
+          },
+        ),
+        appBar: AppBar(
+          title: Text('Lets see some images!'),
+        ),
+      ),
+    );
+  }
+}
